1951df41b6b92511c757409ba08e77217b2fe4e5
callmefrancia <s23120184@alumnos.itsur.edu.mx>
2025-11-30 12:50:58 -0600

    LoginDiseño

--- ProyectoPanaderia/Form1.Designer.cs
+++ ProyectoPanaderia/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             txtUsuario = new TextBox();
             txtContraseña = new TextBox();
             label1 = new Label();
@@ -37,61 +38,68 @@
             // 
             // txtUsuario
             // 
-            txtUsuario.Location = new Point(546, 278);
-            txtUsuario.Margin = new Padding(3, 4, 3, 4);
+            txtUsuario.Font = new Font("Segoe UI", 14.25F);
+            txtUsuario.Location = new Point(488, 288);
             txtUsuario.Name = "txtUsuario";
-            txtUsuario.Size = new Size(268, 27);
+            txtUsuario.Size = new Size(281, 33);
             txtUsuario.TabIndex = 0;
             // 
             // txtContraseña
             // 
-            txtContraseña.Location = new Point(550, 351);
-            txtContraseña.Margin = new Padding(3, 4, 3, 4);
+            txtContraseña.Font = new Font("Segoe UI", 14.25F);
+            txtContraseña.Location = new Point(491, 343);
             txtContraseña.Name = "txtContraseña";
             txtContraseña.PasswordChar = '*';
-            txtContraseña.Size = new Size(268, 27);
+            txtContraseña.Size = new Size(278, 33);
             txtContraseña.TabIndex = 1;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(446, 285);
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Baskerville Old Face", 18F);
+            label1.Location = new Point(390, 291);
             label1.Name = "label1";
-            label1.Size = new Size(59, 20);
+            label1.Size = new Size(86, 27);
             label1.TabIndex = 2;
             label1.Text = "Usuario";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(418, 359);
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Baskerville Old Face", 18F);
+            label2.Location = new Point(366, 346);
             label2.Name = "label2";
-            label2.Size = new Size(83, 20);
+            label2.Size = new Size(117, 27);
             label2.TabIndex = 3;
             label2.Text = "Contraseña";
             // 
             // btnIngresar
             // 
-            btnIngresar.Location = new Point(588, 447);
-            btnIngresar.Margin = new Padding(3, 4, 3, 4);
+            btnIngresar.BackColor = Color.BurlyWood;
+            btnIngresar.BackgroundImageLayout = ImageLayout.Center;
+            btnIngresar.Font = new Font("Baskerville Old Face", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnIngresar.ForeColor = SystemColors.ControlText;
+            btnIngresar.Location = new Point(522, 420);
             btnIngresar.Name = "btnIngresar";
-            btnIngresar.Size = new Size(151, 53);
+            btnIngresar.Size = new Size(132, 40);
             btnIngresar.TabIndex = 4;
             btnIngresar.Text = "Ingresar";
-            btnIngresar.UseVisualStyleBackColor = true;
+            btnIngresar.UseVisualStyleBackColor = false;
             btnIngresar.Click += btnIngresar_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1342, 823);
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(1174, 617);
             Controls.Add(btnIngresar);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(txtContraseña);
             Controls.Add(txtUsuario);
-            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
